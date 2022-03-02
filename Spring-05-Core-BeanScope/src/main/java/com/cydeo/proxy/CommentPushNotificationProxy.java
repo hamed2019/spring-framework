72f70715f4c4a202b11a16df9ef2e91127467295
hamed2019 <hamedhashemi91@yahoo.com>
2022-03-01 21:38:15 -0500

    New changes for spring core

--- Spring-05-Core-BeanScope/src/main/java/com/cydeo/proxy/CommentPushNotificationProxy.java
+++ Spring-05-Core-BeanScope/src/main/java/com/cydeo/proxy/CommentPushNotificationProxy.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 @Qualifier("PUSH")
-class CommentPushNotificationProxy implements CommentNotificationProxy{
+public class CommentPushNotificationProxy implements CommentNotificationProxy{
     @Override
     public void sendComment(Comment comment) {
         System.out.println("Sending push notification for comment : " + comment.getText());
