71208105b852383b0ab7cf3a509d428d96811127
hamedhashemi <hamedhashemi91@yahoo.com>
2022-10-31 20:44:49 -0400

    Spring Orm day 11

---
+++ Spring-10-ORM-TableCreation/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+
+-- INSERT INTO EMPLOYEES(id,name)
+-- VALUES(1,'Mike Smith');
+
+INSERT INTO student(first_name,last_name,email)
+VALUES('Mike',' Smith','mike@cydeo.com');
+
+INSERT INTO student(first_name,last_name,email)
+VALUES('Mohammad',' Hashemi','mohammad@gmail.com');
